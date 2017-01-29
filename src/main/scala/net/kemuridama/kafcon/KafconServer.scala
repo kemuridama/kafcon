a85e8a4caf0bf5504b126552ff5dabbdc07f0a0b
Ryo Ochiai <ryo.ochiai@kemuridama.net>
2017-01-29 10:57:28 +0900

    Introduce rejection handler
    Signed-off-by: Ryo Ochiai <ryo.ochiai@kemuridama.net>

--- src/main/scala/net/kemuridama/kafcon/KafconServer.scala
+++ src/main/scala/net/kemuridama/kafcon/KafconServer.scala
@@ -3,9 +3,12 @@ package net.kemuridama.kafcon
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.{RejectionHandler, MethodRejection}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
 
 import net.kemuridama.kafcon.route._
+import net.kemuridama.kafcon.model.{APIResponse, APIError}
 import net.kemuridama.kafcon.protocol.APIResponseJsonProtocol
 import net.kemuridama.kafcon.util.{UsesApplicationConfig, MixinApplicationConfig}
 
@@ -20,6 +23,11 @@ trait KafconServer
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
   implicit val executitonContext = system.dispatcher
+
+  private implicit val rejectionHandler = RejectionHandler.newBuilder
+    .handle { case MethodRejection(_) => complete(StatusCodes.MethodNotAllowed, APIResponse[Unit](error = Some(APIError(message = Some("Method not allowed"))))) }
+    .handleNotFound { complete(StatusCodes.NotFound, APIResponse[Unit](error = Some(APIError(message = Some("Not found"))))) }
+    .result
 
   private val apiRoutes = pathPrefix("api" / "v1") {
     clustersAPIRoute.route ~
