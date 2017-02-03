f2dbff4dacaa6f459c00e2acbe95c9235e2cef27
Ryo Ochiai <ryo.ochiai@kemuridama.net>
2017-02-04 00:34:22 +0900

    Refactor some methods of cluster model
    Signed-off-by: Ryo Ochiai <ryo.ochiai@kemuridama.net>

--- src/main/scala/net/kemuridama/kafcon/model/Cluster.scala
+++ src/main/scala/net/kemuridama/kafcon/model/Cluster.scala
@@ -14,42 +14,31 @@ case class Cluster(
   private val sessionTimeout = 10 * 1000
   private val connectionTimeout = 10 * 1000
 
-  private var zkUtils: Option[ZkUtils] = None
   private var connectionState: ConnectionState = ConnectionState.Disconnected
 
   private def brokerPath(id: Int) = "/brokers/ids/%d".format(id)
 
-  private def connect: Unit = {
-    zkUtils = Some(ZkUtils(zookeepers.mkString(","), sessionTimeout, connectionTimeout, false))
-  }
-
-  private def disconnect: Unit = {
-    zkUtils.map(_.close)
-    zkUtils = None
-  }
-
-  private def getConnection = zkUtils match {
-    case Some(zk) => Some(zk)
-    case _ => {
-      connect
-      zkUtils
+  private def withSession[T](func: ZkUtils => T): Option[T] = {
+    try {
+      val zkUtils = ZkUtils(zookeepers.mkString(","), sessionTimeout, connectionTimeout, false)
+      val ret = func(zkUtils)
+      zkUtils.close
+      Some(ret)
+    } catch {
+      case _: Throwable => None
     }
-  }
-
-  private def withZkUtils[T](func: ZkUtils => T): Option[T] = getConnection.flatMap { zk =>
-    Some(func(zk))
   }
 
   def getConnectionState: ConnectionState = connectionState
 
-  def getAllBrokers: List[Broker] = withZkUtils { zk =>
+  def getAllBrokers: List[Broker] = withSession { zk =>
     zk.getAllBrokersInCluster.toList.map { broker =>
       val (data, stat) = zk.readDataMaybeNull(brokerPath(broker.id))
       data.map(_.parseJson.convertTo[ZooKeeperBroker].toBroker(broker.id))
     } flatten
   } getOrElse(List.empty[Broker])
 
-  def getAllTopics: List[String] = withZkUtils { zk =>
+  def getAllTopics: List[String] = withSession { zk =>
     zk.getAllTopics.toList
   } getOrElse(List.empty[String])
 
