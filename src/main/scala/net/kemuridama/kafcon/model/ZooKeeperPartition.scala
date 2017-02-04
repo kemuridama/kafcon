4cd93cc048cc492d6c35bd80998bba3be16c6bfa
Ryo Ochiai <ryo.ochiai@kemuridama.net>
2017-02-04 21:01:33 +0900

    Remove zookeeper partiton model and its json protocol
    Signed-off-by: Ryo Ochiai <ryo.ochiai@kemuridama.net>

--- src/main/scala/net/kemuridama/kafcon/model/ZooKeeperPartition.scala
+++
@@ -1,9 +0,0 @@
-package net.kemuridama.kafcon.model
-
-case class ZooKeeperPartition(
-  version: Int,
-  leader: Int,
-  isr: List[Int],
-  controller_epoch: Int,
-  leader_epoch: Int
-)
--- src/main/scala/net/kemuridama/kafcon/protocol/ZooKeeperPartitionJsonProtocol.scala
+++
@@ -1,9 +0,0 @@
-package net.kemuridama.kafcon.protocol
-
-import net.kemuridama.kafcon.model.ZooKeeperPartition
-
-trait ZooKeeperPartitionJsonProtocol extends JsonProtocol {
-
-  implicit val zookeeperPartitionFormat = jsonFormat5(ZooKeeperPartition)
-
-}
